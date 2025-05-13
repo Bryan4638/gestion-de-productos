9b5d528fdf3a3cc8f0e2bde9fb9a988c2c54f8bd
Bryan4638 <brayancespedes57@gmaail.com>
2025-05-13 16:28:27 -0400

    config

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,28 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["picsum.photos"],
-    // Opcional: si necesitas más dominios
-    // domains: ['picsum.photos', 'otro-dominio.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3000',
+        pathname: '/_next/image/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.traefik.me',
+        port: '',
+        pathname: '/_next/image/**',
+      },
+    ],
+    // Opcional: si necesitas desactivar la optimización en desarrollo
+    unoptimized: process.env.NODE_ENV === 'development',
   },
 };
 
